c1276179199915dd8ed628d05f9958a6e24fb90e
AltaraceO <orialtarace@gmail.com>
2022-03-28 19:02:05 +0300

    cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Grid, Paper } from "@material-ui/core";
+import { Grid, Paper, Typography, Box } from "@material-ui/core";
+import { Web } from "@mui/icons-material";
 import sites from "../data";
 
 export const ProjectCard = () => {
@@ -8,7 +9,25 @@ export const ProjectCard = () => {
       {sites.map((site, idx) => {
         return (
           <Grid key={idx} item xs={4}>
-            <Paper elevation={3}> {site.number}</Paper>
+            <Paper elevation={3}>
+              <img src={site.image} alt="page" className="card-image" />
+              <Box paddingX={1}>
+                <Typography variant="subtitle2" component="h2">
+                  {site.name}
+                </Typography>
+                <Box className="card-link">
+                  <Web sx={{ width: 13 }} />
+                  <Typography variant="body2" component="p">
+                    Link
+                  </Typography>
+                </Box>
+                <Box>
+                  {site.tech.map((item) => {
+                    return <div>{item}</div>;
+                  })}
+                </Box>
+              </Box>
+            </Paper>
           </Grid>
         );
       })}
