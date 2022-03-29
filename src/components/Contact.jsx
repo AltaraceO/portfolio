c987e7c6c0330f799f825ef7833deb52b54ed61c
AltaraceO <orialtarace@gmail.com>
2022-03-29 20:16:12 +0300

    contact information displayed in a card

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import { Card, Box, Typography } from "@material-ui/core";
 
 export const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <Box display="inline-block" width={450}>
+      <Card>
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          sx={{ padding: "1.5rem" }}
+        >
+          <Typography>Phone Number</Typography>
+          <Typography>052-6787456</Typography>
+        </Box>
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          sx={{ padding: "1.5rem" }}
+        >
+          <Typography>eMail</Typography>
+          <Typography>orialtarace@gmail.com</Typography>
+        </Box>
+        <Box>Social Media</Box>
+      </Card>
+    </Box>
+  );
 };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,8 @@
 import * as React from "react";
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 
 const pages = ["About", "Projects", "CV", "Contact"];
@@ -33,33 +31,37 @@ const NavBar = () => {
   };
 
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
+    <AppBar position="static" display="flex">
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box>
           <Typography
-            variant="h6"
             noWrap
             component="div"
-            sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+            variant="h6"
+            sx={{ paddingLeft: "1rem", mr: 2, display: { md: "flex" } }}
           >
             Ori Altarace
           </Typography>
+        </Box>
 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}></Box>
-
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                sx={{ my: 2, display: "block" }}
-                onClick={() => buttonLinks({ page })}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
-        </Toolbar>
-      </Container>
+        <Box sx={{ display: "flex", marginRight: "2rem" }}>
+          {pages.map((page) => (
+            <Button
+              key={page}
+              sx={{ my: 2, display: "block" }}
+              onClick={() => buttonLinks({ page })}
+            >
+              {page}
+            </Button>
+          ))}
+        </Box>
+      </Box>
     </AppBar>
   );
 };
