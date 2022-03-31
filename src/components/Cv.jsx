864dfebf2a59d5ac3ca2eb4d94e3b02c24ed6fde
AltaraceO <orialtarace@gmail.com>
2022-03-31 18:16:43 +0300

    change pdf height and width

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -1,18 +1,19 @@
 import React from "react";
-import { Link, Divider } from "@material-ui/core";
+import { Link, Box } from "@material-ui/core";
 import CV from "../Ori_Altarace_CV.pdf";
 
 export const Cv = () => {
   return (
     <>
-      <object width="100%" height="500" type="application/pdf" data={CV}>
+      <Box>
+        <Link href={CV} download>
+          Download as PDF
+        </Link>
+      </Box>
+      <br />
+      <object width="400" height="500" type="application/pdf" data={CV}>
         <p>pdf</p>
       </object>
-
-      <Divider />
-      <Link href={CV} download>
-        Download as PDF
-      </Link>
     </>
   );
 };
