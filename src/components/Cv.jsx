e60fddbe4231526aa19c3ee24c0c08f9fb4f0482
AltaraceO <orialtarace@gmail.com>
2022-03-31 18:12:54 +0300

    display pdf using an object  iframe embed options

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -5,7 +5,9 @@ import CV from "../Ori_Altarace_CV.pdf";
 export const Cv = () => {
   return (
     <>
-      <embed src={CV} width="100%" height="1000px" />
+      <object width="100%" height="500" type="application/pdf" data={CV}>
+        <p>pdf</p>
+      </object>
 
       <Divider />
       <Link href={CV} download>
