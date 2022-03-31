6cb012b7cb3225a8d23682f8f9bd42852aa35d1f
AltaraceO <orialtarace@gmail.com>
2022-03-31 16:56:21 +0300

    todo with the about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,4 +2,6 @@ import React from "react";
 
 export const About = () => {
   return <div>About</div>;
+  //TODO add a link to the CONTACT at the end and write something like
+  //TODO "to get in touch..."
 };
