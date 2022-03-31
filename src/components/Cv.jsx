6499353eaa9511f26f495755faf68eb7b878125b
AltaraceO <orialtarace@gmail.com>
2022-03-31 17:38:34 +0300

    download CV

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { Link, Divider } from "@material-ui/core";
+import CV from "../Ori_Altarace_CV.pdf";
 
 export const Cv = () => {
-  return <div>This will display the cv</div>;
+  return (
+    <>
+      <embed src={CV} width="100%" height="1000px" />
+
+      <Divider />
+      <Link href={CV} download>
+        Download as PDF
+      </Link>
+    </>
+  );
 };
