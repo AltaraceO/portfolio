71eaff92122440e5740ebc337e5bdf8366a3c5c0
AltaraceO <orialtarace@gmail.com>
2022-04-03 20:17:58 +0300

    changed justify to justifyContent on the MUI grid

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,7 +4,7 @@ import { ProjectCard } from "./ProjectCard";
 
 export const Cards = () => {
   return (
-    <Grid container justify="center" spacing={5}>
+    <Grid container justifyContent="center" spacing={5}>
       <ProjectCard />
     </Grid>
   );
