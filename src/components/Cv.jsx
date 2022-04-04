5fa7570d96e329c83754092f5a5651e3acbf7618
AltaraceO <orialtarace@gmail.com>
2022-04-04 14:41:17 +0300

    cv pdf positioning

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -2,16 +2,32 @@ import React from "react";
 import { Link, Box } from "@material-ui/core";
 import CV from "../Ori_Altarace_CV.pdf";
 
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  pdfObject: {
+    display: "block",
+    margin: "0 auto",
+    width: 400,
+    height: 500,
+    [theme.breakpoints.down("xs")]: {
+      width: 200,
+      height: 300,
+    },
+  },
+}));
+
 export const Cv = () => {
+  const classes = useStyles();
   return (
     <>
       <Box>
         <Link href={CV} download>
-          Download as PDF
+          Download CV
         </Link>
       </Box>
       <br />
-      <object width="400" height="500" type="application/pdf" data={CV}>
+      <object className={classes.pdfObject} type="application/pdf" data={CV}>
         <p>pdf</p>
       </object>
     </>
