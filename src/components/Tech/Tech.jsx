45a8da5ae9c2d8803918b5bef7e4bb5708adf880
AltaraceO <orialtarace@gmail.com>
2022-04-10 11:39:17 +0300

    icon color match

--- src/components/Tech/Tech.jsx
+++ src/components/Tech/Tech.jsx
@@ -19,26 +19,26 @@ export const Tech = () => {
     <div className="tech-container">
       <IconContext.Provider value={{ size: "2rem" }}>
         <TechBox
-          icon={<SiJavascript style={{ color: "red" }} />}
+          icon={<SiJavascript style={{ color: "#E8D44D" }} />}
           name="JavaScript"
         />
-        <TechBox icon={<SiHtml5 style={{ color: "blue" }} />} name="HTML5" />
-        <TechBox icon={<SiCss3 style={{ color: "purple" }} />} name="CSS3" />
-        <TechBox icon={<SiReact style={{ color: "purple" }} />} name="React" />
+        <TechBox icon={<SiHtml5 style={{ color: "#E96227" }} />} name="HTML5" />
+        <TechBox icon={<SiCss3 style={{ color: "#146EB0" }} />} name="CSS3" />
+        <TechBox icon={<SiReact style={{ color: "#5ED3F3" }} />} name="React" />
         <TechBox
-          icon={<FaNodeJs style={{ color: "purple" }} />}
+          icon={<FaNodeJs style={{ color: "#7CB703" }} />}
           name="NodeJs"
         />
         <TechBox
-          icon={<SiExpress style={{ color: "purple" }} />}
+          icon={<SiExpress style={{ color: "#333333" }} />}
           name="Express"
         />
         <TechBox
-          icon={<SiMongodb style={{ color: "purple" }} />}
+          icon={<SiMongodb style={{ color: "#569034" }} />}
           name="MongoDB"
         />
         <TechBox
-          icon={<SiMaterialui style={{ color: "purple" }} />}
+          icon={<SiMaterialui style={{ color: "#007DC4" }} />}
           name="MaterialUI"
         />
       </IconContext.Provider>
