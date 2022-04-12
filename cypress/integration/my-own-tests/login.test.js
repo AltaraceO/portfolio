f5aeeb74406fc11376bf2de485488e39a826fc14
Ori Altarace <orialtarace@gmail.com>
2022-04-12 10:48:58 +0300

    adding auth token to local storage in tests

--- cypress/integration/my-own-tests/login.test.js
+++ cypress/integration/my-own-tests/login.test.js
@@ -1,6 +1,13 @@
 /// <reference types="cypress" />
+const token = "astringoflettersandnumbersthatwilltogethercompriseatoken";
 
 describe("basic desktop test", () => {
+  //set token once before instead of before each
+  before(() => {
+    cy.then(() => {
+      window.localStorage.setItem("__auth__token", token);
+    });
+  });
   //this will run before each test is run. So there is no need to repeat this code over and over
   beforeEach(() => {
     cy.viewport(1280, 720);
